512124f1fbdd0cfa5cb9908dba34d62131e3997f
Sergiu Nagy <sergiunb@gmail.com>
2020-10-26 16:16:09 +0100

    Integrate SpringBoot setup project. Closes #ACTtrack-3

---
+++ actrack/src/main/java/sena/activitytracker/actrack/ActrackApplication.java
@@ -0,0 +1,13 @@
+package sena.activitytracker.actrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActrackApplication.class, args);
+	}
+
+}
---
+++ actrack/src/test/java/sena/activitytracker/actrack/ActrackApplicationTests.java
@@ -0,0 +1,13 @@
+package sena.activitytracker.actrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
