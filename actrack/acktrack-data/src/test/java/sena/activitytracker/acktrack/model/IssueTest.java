e1eff98fa307052372bfaf6c6b82b3e13930c0ef
Sergiu Nagy <sergiunb@gmail.com>
2020-11-01 17:21:47 +0100

    Tests for Issue POJO . Closes ACTRACK-53

---
+++ actrack/acktrack-data/src/test/java/sena/activitytracker/acktrack/model/IssueTest.java
@@ -0,0 +1,104 @@
+package sena.activitytracker.acktrack.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class IssueTest {
+
+    Issue issue;
+
+    @BeforeEach
+    void setUp() {
+        issue = Issue.builder()
+                .id(1L)
+                .issue_id("14a8")
+                .description("quality issue alpha")
+                .link("url quality")
+                .build();
+    }
+
+    @Test
+    void addWorkpackages() {
+        // given
+        final String TXT1 = "first";
+        final String TXT2 = "2nd ";
+        Set<Workpackage> workpackages = new HashSet<>();
+        workpackages.add(Workpackage.builder().id(1L).description(TXT1).build());
+        workpackages.add(Workpackage.builder().id(2L).description(TXT2).build());
+
+        // when
+        Set<Workpackage> boundWorkpackages = issue.addWorkpackages(workpackages);
+
+        // then
+        assertNotNull(issue.getWorkpackages());
+        assertEquals(2, issue.getWorkpackages().size());
+        assertTrue(issue.getWorkpackages().stream().anyMatch(user -> user.getDescription().equals(TXT1)));
+        assertTrue(issue.getWorkpackages().stream().anyMatch(user -> user.getDescription().equals(TXT2)));
+
+        assertNotNull(boundWorkpackages);
+        Optional<Workpackage> workpackageOptional1 = boundWorkpackages.stream().filter(workpackage1 -> workpackage1.getDescription().equals(TXT1)).findFirst();
+        Optional<Workpackage> workpackageOptional2 = boundWorkpackages.stream().filter(workpackage1 -> workpackage1.getDescription().equals(TXT2)).findFirst();
+        assertTrue(workpackageOptional1.isPresent());
+        assertTrue(workpackageOptional2.isPresent());
+        // check directly on objects
+        assertTrue(workpackageOptional1.get().getIssues().stream().anyMatch(issue1 -> issue1 == issue));
+        assertTrue(workpackageOptional2.get().getIssues().stream().anyMatch(issue1 -> issue1 == issue));
+
+    }
+
+    @Test
+    void addWorkpackage() {
+        // given
+        final String TXT1 = "first";
+        Workpackage workpackage = Workpackage.builder().id(1L).description(TXT1).build();
+
+        // when
+        issue.addWorkpackage(workpackage);
+
+        // then
+        assertNotNull(issue.getWorkpackages());
+        assertEquals(1, issue.getWorkpackages().size());
+        assertTrue(issue.getWorkpackages().stream().anyMatch(workpackage1 -> workpackage1.getDescription().equals(TXT1)));
+
+        // check directly on objects
+        assertTrue(workpackage.getIssues().stream().anyMatch(issue1 -> issue1 == issue));
+    }
+
+    @Test
+    void addActivities() {
+        // given
+        final String TXT1 = "first";
+        final String TXT2 = "2nd ";
+        Set<Activity> activities = new HashSet<>();
+        activities.add(Activity.builder().id(1L).description(TXT1).build());
+        activities.add(Activity.builder().id(2L).description(TXT2).build());
+
+        // when
+        Set<Activity> boundWorkpackages = issue.addActivities(activities);
+
+        // then
+        assertNotNull(issue.getActivities());
+        assertEquals(2, issue.getActivities().size());
+        assertTrue(issue.getActivities().stream().anyMatch(activity -> activity.getDescription().equals(TXT1)));
+        assertTrue(issue.getActivities().stream().anyMatch(activity -> activity.getDescription().equals(TXT2)));
+
+        assertNotNull(boundWorkpackages);
+        Optional<Activity> activityOptional1 = boundWorkpackages.stream().filter(activity -> activity.getDescription().equals(TXT1)).findFirst();
+        Optional<Activity> activityOptional2 = boundWorkpackages.stream().filter(activity -> activity.getDescription().equals(TXT2)).findFirst();
+        assertTrue(activityOptional1.isPresent());
+        assertTrue(activityOptional2.isPresent());
+
+        // check directly on objects
+        assertTrue(activityOptional1.get().getIssues().stream().anyMatch(issue1 -> issue1 == issue));
+        assertTrue(activityOptional2.get().getIssues().stream().anyMatch(issue1 -> issue1 == issue));
+    }
+
+
+}
