01deee7e38f5d25de1073a29af949a6be3eb1b59
Sergiu Nagy <sergiunb@gmail.com>
2020-11-04 15:14:05 +0100

    Add tests for Activity save with 500 character text. Closes ACTRACK-80

--- actrack/acktrack-web/src/test/java/sena/activitytracker/acktrack/integration/services/ActivitiesServiceIT.java
+++ actrack/acktrack-web/src/test/java/sena/activitytracker/acktrack/integration/services/ActivitiesServiceIT.java
@@ -11,12 +11,17 @@ import sena.activitytracker.acktrack.repositories.ActivityRepository;
 import sena.activitytracker.acktrack.services.ActivityService;
 import sena.activitytracker.acktrack.services.ActivityServiceImpl;
 
+import javax.swing.text.html.Option;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -40,7 +45,7 @@ public class ActivitiesServiceIT {
 
 
     @Test
-    void testLoadActivitiesBetweenDates(){
+    void testLoadActivitiesBetweenDates() {
         int TEN = 10;
 
         saveNActivitiesToRepository(TEN);
@@ -48,7 +53,7 @@ public class ActivitiesServiceIT {
         Set<Activity> databaseActivities = new HashSet<>();
         activityService.findAll().forEach(databaseActivities::add);
         assertEquals(10, databaseActivities.size());
-        Set<Activity> datedActivities = activityService.findAllByDateBetween(LocalDate.now().minusDays(5), LocalDate.now() );
+        Set<Activity> datedActivities = activityService.findAllByDateBetween(LocalDate.now().minusDays(5), LocalDate.now());
 
         System.out.println(datedActivities);
     }
@@ -69,6 +74,32 @@ public class ActivitiesServiceIT {
                 }
         );
         activityService.saveAll(activities);
+    }
+
+    @Test
+    void saveLargeTextActivity() {
+
+        String text500char = "Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio " +
+                "urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper " +
+                "pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla non arcu lacinia neque faucibus fringilla." +
+                " Nulla non lectus sed nisl molestie malesuada. Proin in tellus sit amet nibh dignissim sagittis. " +
+                "Vivamus luctus egestas leo. Maecenas sollicitudin. Nullam rhoncus aliquam met";
+
+        Activity largeTextActivity = Activity.builder()
+                .id(1L)
+                .duration(Duration.of(8, ChronoUnit.HOURS))
+                .date(LocalDate.of(2020, 8, 14))
+                .description(text500char)
+                .isExported(false)
+                .build();
+
+        activityService.save(largeTextActivity);
+
+        Optional<Activity> foundOptional = activityService.findById(1L);
+
+        assertTrue(foundOptional.isPresent());
+        assertTrue(text500char.equals(foundOptional.get().getDescription()));
+
     }
 
 }
