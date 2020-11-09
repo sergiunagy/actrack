1cd1f2f28fc24a23ac0d7bd1997fec3bac42d2e7
Sergiu Nagy <sergiunb@gmail.com>
2020-11-09 21:27:40 +0100

    Add Vaadin UI module to the project. ACTRACK-108

---
+++ actrack/acktrack-vaadinui/src/main/java/sena/actrack/vaadin/vaadinui/VaadinuiApplication.java
@@ -0,0 +1,13 @@
+package sena.actrack.vaadin.vaadinui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaadinuiApplication.class, args);
+	}
+
+}
