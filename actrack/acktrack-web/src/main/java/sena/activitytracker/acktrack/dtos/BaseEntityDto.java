7dd4ddb29d74b727d3d9d1455d4552ab853b9bac
Sergiu Nagy <sergiunb@gmail.com>
2021-01-26 00:13:03 +0100

    Create BaseEntityDTO to map n the basic entity ancestor class.Closes ACTRACK-189

---
+++ actrack/acktrack-web/src/main/java/sena/activitytracker/acktrack/dtos/BaseEntityDto.java
@@ -0,0 +1,21 @@
+package sena.activitytracker.acktrack.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+public class BaseEntityDto {
+
+    /*READ ONLY VALUES*/
+    private UUID id;
+    private Long version;
+    private Timestamp createdTimestamp;
+    private Timestamp updatedTimestamp;
+
+}
