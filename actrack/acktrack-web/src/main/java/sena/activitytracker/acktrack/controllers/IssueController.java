6f3f660a1f5651497234380705c380063f4313b5
Sergiu Nagy <sergiunb@gmail.com>
2021-02-08 20:31:07 +0100

    Update issues controllerand tests. Closes ACTRACK-256.

--- actrack/acktrack-web/src/main/java/sena/activitytracker/acktrack/controllers/IssueController.java
+++ actrack/acktrack-web/src/main/java/sena/activitytracker/acktrack/controllers/IssueController.java
@@ -1,9 +1,31 @@
 package sena.activitytracker.acktrack.controllers;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import sena.activitytracker.acktrack.model.Issue;
+import sena.activitytracker.acktrack.services.IssueService;
+
+import java.util.Set;
 
 @Slf4j
 @Controller
+@AllArgsConstructor
 public class IssueController {
+    public static final String LIST_ISSUES_PAGE = "/issues/issues_list";
+
+    private final IssueService issueService;
+
+    @GetMapping("/list_issues")
+    public String listIssues(Model model){
+
+        Set<Issue> issueSet = issueService.findAll();
+        /*TODO : to DTO list ordered by creation date*/
+        model.addAttribute("issues", issueSet);
+
+        return LIST_ISSUES_PAGE;
+    }
 }
