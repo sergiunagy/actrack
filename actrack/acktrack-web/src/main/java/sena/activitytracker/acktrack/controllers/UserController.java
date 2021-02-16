b11c0ac428e43429a8d66e3394f008261a8f05a4
Sergiu Nagy <sergiunb@gmail.com>
2021-02-16 19:00:45 +0100

    Create Controller and basic linking for the Users list. Updates ACTRACK-278.

--- actrack/acktrack-web/src/main/java/sena/activitytracker/acktrack/controllers/UserController.java
+++ actrack/acktrack-web/src/main/java/sena/activitytracker/acktrack/controllers/UserController.java
@@ -1,4 +1,30 @@
 package sena.activitytracker.acktrack.controllers;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import sena.activitytracker.acktrack.model.security.User;
+import sena.activitytracker.acktrack.services.security.UserService;
+
+import java.util.Set;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
 public class UserController {
+
+    private static final String LIST_USERS_PAGE = "/users/users_list";
+
+    private final UserService userService;
+
+    @GetMapping("/list_teammates")
+    public String listUsers(Model model){
+
+        Set<User> users = userService.findAll();
+
+        model.addAttribute("users", users);
+        return LIST_USERS_PAGE;
+    }
 }
