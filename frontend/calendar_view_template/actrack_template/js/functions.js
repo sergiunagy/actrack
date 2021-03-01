abfa5bfb13b885a92b88eb53c5799e2887c2e5c9
Sergiu Nagy <sergiunb@gmail.com>
2021-03-01 23:09:28 +0100

    Work on frontend template for the timesheet-calendar view. Remaining : monthly metrics and fix positioning on re-size. Updates ACTRACK-294

--- frontend/calendar_view_template/actrack_template/js/functions.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const sidebar = document.querySelector('.sidebar');
-const btn_sidebar_colapse = document.querySelector('#sidebarCollapse');
-
-btn_sidebar_colapse.addEventListener('click', ()=>  sidebar.classList.toggle('active'));
